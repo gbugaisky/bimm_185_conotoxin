2c8329676e64f3aba3739d94b9c213c5c38eba40
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2014-04-19 18:17:18 -0700

    Adding multiple sequence parser before it gets too complicated

---
+++ consensus/procToMult.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import tempfile
+import os
+from sys import argv
+
+def parser(filename, charstart):
+	#first, get a line count to properly space the sequences
+	with open("unproc\\" + filename, 'r') as counting:
+		for count, l in enumerate(counting, 1):
+			pass
+
+	#set max spacing, with default of 1
+	maxSpacing = 1
+	while (count >= 1):
+		maxSpacing += 1
+		count /= 10
+	print maxSpacing
+
+	# Start processing the file
+	with open("unproc\\" + filename, 'r') as inf, tempfile.NamedTemporaryFile(delete=False) as outf:
+		i = 0
+		for line in inf:
+
+			# Write the file header
+			if i is 0:
+				seqChar = len(line) - int(charstart)
+				for idx in range(0, maxSpacing):
+					outf.write(' ')
+				outf.write('1')
+				for idx in range(1, seqChar):
+					outf.write(' ')
+				outf.write(str(seqChar) + '\n')
+				i += 1
+
+			if (i % 10)
+			#write the sequence number, followed by spaces, followed by the sequence
+			outf.write(str(i))
+			outf.write(str(i)) " " + line[int(charstart):])
+			i += 1
+		outname = outf.name
+	try:
+		os.remove("multi\\" + filename)
+	except OSError:
+		pass
+	os.rename(outname, "multi\\temp" + filename)
+
+if __name__ == "__main__":
+	parser(argv[1], argv[2])
