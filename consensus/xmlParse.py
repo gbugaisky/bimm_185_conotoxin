22f528bdda5e9e5b2ba5374cf6eed01fc2b86437
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2014-04-23 12:44:54 -0700

    Finished database parser

--- consensus/xmlParse.py
+++ consensus/xmlParse.py
@@ -1,27 +1,60 @@
 #!/usr/bin/env python
 
+import json
+import os
 from time import clock
+
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
 
 def parseConotoxinXML(xmlFile):
+    SEQUENCES_PATH = ".\\superSequences\\"
+    SEQUENCE_THRESHOLD = 5
+
+    #create the sequence directory
+    if not os.path.exists(SEQUENCES_PATH):
+        os.makedirs(SEQUENCES_PATH)
+
+    # Take the XML database and 'import' it
     start = clock()
     tree = ET.ElementTree(file=xmlFile)
     end = clock()
     print "Load time of Database: " + str(end - start)
+    superCount = {}
     tree.getroot()
-    fileAsup = open('A_superfamily', 'w')
+
+    # Create dictionary of all superfamilies
     for element in tree.findall('entry'):
         try:
             superfamily = element.find('geneSuperfamily').text
-            if superfamily == "A superfamily" and ('precursor' in element.find('name').text):
-                fileAsup.write(element.find('sequence').text + '\n')
+            if 'precursor' in element.find('name').text:
+                superCount[superfamily] = superCount.get(superfamily, 0) + 1
         except AttributeError:
             continue
-    #    fileAsup.write(element.tag + element.attrib + "\n")
-    fileAsup.close()
+
+    # This file only exists for debugging and analytic purposes
+    with open('superfamily_count.txt', 'w') as supFile:
+        supFile.write(json.dumps(superCount))
+
+    # now, create the superfamily files in a pseudoFASTA format
+    start = clock()
+    for familyName, count in superCount.iteritems():
+        if count >= SEQUENCE_THRESHOLD:
+            with open(SEQUENCES_PATH + familyName + ".fasta", 'w') as f:
+                for element in tree.findall('entry'):
+                    try:
+                        superfamily = element.find('geneSuperfamily').text
+                        if superfamily == familyName and 'precursor' in element.find('name').text:
+                            f.write("> " + element.find('name').text + '\n')
+                            f.write(element.find('sequence').text + '\n\n')
+                    except AttributeError:
+                        continue
+    end = clock()
+    print "Superfamily Files Creation Time: " + str(end - start)
+
+
 
 if __name__ == "__main__":
     parseConotoxinXML("conoserver_protein.xml")
