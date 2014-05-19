61c6b11ee4e59ea7bb4575355da47e8c5061c09f
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2014-05-19 14:41:18 -0700

    Changed Test & Train generator to produce 10 sets of files, also changed Average kNN Accuracy calculation

--- consensus/kNNProc.py
+++ consensus/kNNProc.py
@@ -5,20 +5,34 @@ import pylab as pl
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors
 
-def kNNGen(filename):
-	features = np.genfromtxt(filename, delimiter=' ', usecols=(0, 1, 2))
-	labels = np.genfromtxt(filename, delimiter=' ', usecols=(-1))
+def kNNGen(trainfile, testfile):
+	features = np.genfromtxt(trainfile, delimiter=' ', usecols=(0, 1, 2))
+	labels = np.genfromtxt(trainfile, delimiter=' ', usecols=(-1))
+	tests = np.genfromtxt(testfile, delimiter=' ', usecols=(0, 1, 2))
+	testlabels = np.genfromtxt(testfile, delimiter=' ', usecols=(-1))
 
 	n_neighbors = 10
 
 	h = 0.02
-
+	accuracyScores = []
 	for weights in ['uniform', 'distance']:
 		clf = neighbors.KNeighborsClassifier(n_neighbors, leaf_size=20, weights=weights)
 		clf.fit(features, labels)
-		print clf.get_params(deep=True)
-		print clf.score(features, labels)
-		print clf.kneighbors_graph(features)
+		accuracyScores.append(clf.score(tests, testlabels))
+	return accuracyScores
 
 if __name__ == "__main__":
-	kNNGen(".\\MachineParse\\mass_isoElectricPoint_cysAvg.csv")
+	FILEPATH = ".\\SeparatedTrainTest\\"
+	accuracyVals = []
+	for i in range(0, 10):
+		accuracyVals.append(kNNGen(FILEPATH + "trainDataSet" + str(i) + ".csv", FILEPATH + "testDataSet" + str(i) + ".csv"))
+	uniformScore = 0
+	distanceScore = 0
+	with open("kNNAverageAccuracy.txt", 'w') as results:
+		for element in accuracyVals:
+			results.write(str(element) + '\n')
+			uniformScore += element[0]
+			distanceScore += element[1]
+
+		results.write("Uniform kNN Accuracy: " + str(uniformScore / 10.0) + '\n')
+		results.write("Distance kNN Accuracy: " + str(distanceScore / 10.0) + '\n')
--- consensus/parseToTrainTestSets.py
+++ consensus/parseToTrainTestSets.py
@@ -1,20 +1,31 @@
 #!usr/bin/env/python
+from os import path, makedirs
 
 def parseToTrainTestSets(filename):
-	conoData = [[] for i in range(5)]
-	with open(filename, 'r') as data:
-		for line in data:
-			conoData[int(line[-2])].append(line)
+    FILEPATH = ".\\SeparatedTrainTest\\"
+    if not path.exists(FILEPATH):
+        makedirs(FILEPATH)
 
-	with open('trainData.csv', 'w') as training, open('testData.csv', 'w') as testing:
-		for label in conoData:
-			threshold = int(len(label) * 0.9)
-			print threshold
-			for idx, row in enumerate(label):
-				if idx < threshold:
-					training.write(row)
-				else:
-					testing.write(row)
+    conoData = [[] for i in range(5)]
+    with open(filename, 'r') as data:
+        for line in data:
+            conoData[int(line[-2])].append(line)
+
+    for i in range(0, 10):
+        print i
+        with open(FILEPATH + 'trainDataSet' + str(i) + '.csv', 'w') as training, open(FILEPATH + 'testDataSet' + str(i) + '.csv', 'w') as testing:
+            for label in conoData:
+                threshold = int(len(label) * 0.1 * i)
+                threshLimit = threshold + int(len(label) * 0.1)
+                if threshLimit == threshold:
+                    threshLimit += 1
+                for idx, row in enumerate(label):
+                    if idx < threshold:
+                        training.write(row)
+                    elif idx >= threshold and idx < threshLimit:
+                        testing.write(row)
+                    else:
+                        training.write(row)
 
 if __name__ == "__main__":
-	parseToTrainTestSets(".\\MachineParse\\mass_isoElectricPoint_cysAvg.csv")
+    parseToTrainTestSets(".\\MachineParse\\mass_isoElectricPoint_cysAvg.csv")
